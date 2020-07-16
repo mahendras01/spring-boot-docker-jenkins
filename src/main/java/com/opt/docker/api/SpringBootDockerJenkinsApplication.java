6a7e51c12a854895523a596e8be640683f264019
sjh <sahu.mahendra0101@gmail.com>
2020-07-16 09:05:17 +0530

    First git commit

---
+++ src/main/java/com/opt/docker/api/SpringBootDockerJenkinsApplication.java
@@ -0,0 +1,21 @@
+package com.opt.docker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerJenkinsApplication {
+	
+	@GetMapping("/message")
+	public String home() {
+		return "Welcome to AWS First deployent...!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opt/docker/api/SpringBootDockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opt.docker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
